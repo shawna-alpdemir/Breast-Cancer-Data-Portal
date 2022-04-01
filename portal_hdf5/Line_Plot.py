573914ea0d67f49c7b0efa959d85b2bd57c7a25f
kenlzh1996 <zl292@cornell.edu>
2022-03-31 20:57:31 -0400

    emm

--- portal_hdf5/Line_Plot.py
+++ portal_hdf5/Line_Plot.py
@@ -23,57 +23,30 @@ INITIAL_GENE = ['NDUFS2', 'NDUFS3', 'NDUFS7', 'blank 4']
 GENE_COLORS = ['red', 'blue', 'green', 'orange']
 
 def Johansson_Line_Plot():
-    # Initializing the figure object
+    """Initializing the line plot"""
     jo_plot_p = figure(x_range=FactorRange(*johansson_subtype_tumor_tuple), title='', x_axis_label='',
                        y_axis_label='protein z-score', plot_width=LINE_PLOT_WIDTH, plot_height=LINE_PLOT_HEIGHT)
     jo_plot_m = figure(x_range=FactorRange(*johansson_subtype_tumor_tuple), title='', x_axis_label='',
                        y_axis_label='mRNA z-score', plot_width=LINE_PLOT_WIDTH, plot_height=LINE_PLOT_HEIGHT)
 
-    for j in range(4):
-        jo_plot_p.line(subtype_tuple, protein_data, source=johansson_cds[j], color=GENE_COLORS[j],
-                       legend_label=GENE_NUMBER[j])
-        jo_plot_p.circle(subtype_tuple, protein_data, source=johansson_cds[j], color=GENE_COLORS[j], size=4,
-                         legend_label=GENE_NUMBER[j])
-        jo_plot_m.line(subtype_tuple, mRNA_data, source=johansson_cds[j], color=GENE_COLORS[j],
-                       legend_label=GENE_NUMBER[j])
-        jo_plot_m.circle(subtype_tuple, mRNA_data, source=johansson_cds[j], color=GENE_COLORS[j], size=4,
-                         legend_label=GENE_NUMBER[j])
-
     return jo_plot_p, jo_plot_m
 
 
 def Krug_Line_Plot():
+    """Initializing the line plot"""
     kr_plot_p = figure(x_range=FactorRange(*krug_subtype_tumor_tuple), title='', x_axis_label='',
                        y_axis_label='protein z-score', plot_width=LINE_PLOT_WIDTH, plot_height=LINE_PLOT_HEIGHT)
     kr_plot_m = figure(x_range=FactorRange(*krug_subtype_tumor_tuple), title='', x_axis_label='',
                        y_axis_label='mRNA z-score', plot_width=LINE_PLOT_WIDTH, plot_height=LINE_PLOT_HEIGHT)
 
-    for j in range(4):
-        kr_plot_p.line(subtype_tuple, protein_data, source=krug_cds[j], color=GENE_COLORS[j],
-                       legend_label=GENE_NUMBER[j])
-        kr_plot_p.circle(subtype_tuple, protein_data, source=krug_cds[j], color=GENE_COLORS[j], size=4,
-                         legend_label=GENE_NUMBER[j])
-        kr_plot_m.line(subtype_tuple, mRNA_data, source=krug_cds[j], color=GENE_COLORS[j], legend_label=GENE_NUMBER[j])
-        kr_plot_m.circle(subtype_tuple, mRNA_data, source=krug_cds[j], color=GENE_COLORS[j], size=4,
-                         legend_label=GENE_NUMBER[j])
-
     return kr_plot_p, kr_plot_m
 
 
 def Mertins_Line_Plot():
+    """Initializing the line plot"""
     me_plot_p = figure(x_range=FactorRange(*mertins_subtype_tumor_tuple), title='', x_axis_label='',
                        y_axis_label='protein z-score', plot_width=LINE_PLOT_WIDTH, plot_height=LINE_PLOT_HEIGHT)
     me_plot_m = figure(x_range=FactorRange(*mertins_subtype_tumor_tuple), title='', x_axis_label='',
                        y_axis_label='mRNA z-score', plot_width=LINE_PLOT_WIDTH, plot_height=LINE_PLOT_HEIGHT)
-
-    for j in range(4):
-        me_plot_p.line(subtype_tuple, protein_data, source=mertins_cds[j], color=GENE_COLORS[j],
-                       legend_label=GENE_NUMBER[j])
-        me_plot_p.circle(subtype_tuple, protein_data, source=mertins_cds[j], color=GENE_COLORS[j], size=4,
-                         legend_label=GENE_NUMBER[j])
-        me_plot_m.line(subtype_tuple, mRNA_data, source=mertins_cds[j], color=GENE_COLORS[j],
-                       legend_label=GENE_NUMBER[j])
-        me_plot_m.circle(subtype_tuple, mRNA_data, source=mertins_cds[j], color=GENE_COLORS[j], size=4,
-                         legend_label=GENE_NUMBER[j])
 
     return me_plot_p, me_plot_m
--- portal_hdf5/Scatter_Plot.py
+++ portal_hdf5/Scatter_Plot.py
@@ -14,6 +14,7 @@ GENE1 = 'NDUFS2'
 GENE2 = 'NDUFS3'
 GENE3 = 'NDUFS7'
 GENE4 = 'blank 4'
+INITIAL_GENE = ['NDUFS2', 'NDUFS3', 'NDUFS7', 'blank 4']
 five_subtypes = ['Basal', 'Her2', 'LumA', 'LumB', 'Norm']  # list of subtypes
 five_subtype_colors = ['#E31A1C', '#FB9A99', '#1F78B4', '#A6CEE3','#33A02C']
 
@@ -32,119 +33,70 @@ gene = 'gene'
 def Pro_Pro_Scatter_Plot():
     """protein-protein for gene 1 and gene 2 scatter plot for Johansson, Krug and Mertins"""
 
-    jo_pro_pro_scatter_plot = figure(title='', x_axis_label=f'{GENE1} protein z-score', y_axis_label=f'{GENE2} protein z-score',
+    jo_pro_pro_scatter_plot = figure(title='NDUFS2-NDUFS3', x_axis_label='protein z-score', y_axis_label='protein z-score',
                                 plot_width=SCATTER_PLOT_WIDTH, plot_height=SCATTER_PLOT_HEIGHT)
-    jo_pro_pro_scatter_plot.scatter('x_protein_data', 'y_protein_data', source=johansson_cds[4],
-                               color=factor_cmap(subtype, five_subtype_colors, five_subtypes), legend_group=subtype)
-
-    kr_pro_pro_scatter_plot = figure(title='', x_axis_label=f'{GENE1} protein z-score', y_axis_label=f'{GENE2} protein z-score',
+    kr_pro_pro_scatter_plot = figure(title='NDUFS2-NDUFS3', x_axis_label='protein z-score', y_axis_label='protein z-score',
                                 plot_width=SCATTER_PLOT_WIDTH, plot_height=SCATTER_PLOT_HEIGHT)
-    kr_pro_pro_scatter_plot.scatter('x_protein_data', 'y_protein_data', source=krug_cds[4],
-                               color=factor_cmap(subtype, five_subtype_colors, five_subtypes), legend_group=subtype)
-
-    me_pro_pro_scatter_plot = figure(title='', x_axis_label=f'{GENE1} protein z-score', y_axis_label=f'{GENE2} protein z-score',
+    me_pro_pro_scatter_plot = figure(title='NDUFS2-NDUFS3', x_axis_label='protein z-score', y_axis_label='protein z-score',
                                 plot_width=SCATTER_PLOT_WIDTH, plot_height=SCATTER_PLOT_HEIGHT)
-    me_pro_pro_scatter_plot.scatter('x_protein_data', 'y_protein_data', source=mertins_cds[4],
-                                    color=factor_cmap(subtype, five_subtype_colors[0:4], five_subtypes[0:4]), legend_group=subtype)
 
     return jo_pro_pro_scatter_plot, kr_pro_pro_scatter_plot, me_pro_pro_scatter_plot
 
 def RNA_RNA_Scatter_Plot():
     """RNA-RNA for gene 1 and gene 2 scatter plot for Johansson, Krug and Mertins"""
 
-    jo_rna_rna_scatter_plot = figure(title='', x_axis_label=f'{GENE1} mRNA z-score', y_axis_label=f'{GENE2} mRNA z-score',
+    jo_rna_rna_scatter_plot = figure(title='NDUFS2-NDUFS3', x_axis_label='mRNA z-score', y_axis_label='mRNA z-score',
                                 plot_width=SCATTER_PLOT_WIDTH, plot_height=SCATTER_PLOT_HEIGHT)
-    jo_rna_rna_scatter_plot.scatter('x_mRNA_data', 'y_mRNA_data', source=johansson_cds[5],
-                                color = factor_cmap(subtype, five_subtype_colors, five_subtypes), legend_group = subtype)
-
-    kr_rna_rna_scatter_plot = figure(title='', x_axis_label=f'{GENE1} mRNA z-score', y_axis_label=f'{GENE2} mRNA z-score',
+    kr_rna_rna_scatter_plot = figure(title='NDUFS2-NDUFS3', x_axis_label='mRNA z-score', y_axis_label='mRNA z-score',
                                 plot_width=SCATTER_PLOT_WIDTH, plot_height=SCATTER_PLOT_HEIGHT)
-    kr_rna_rna_scatter_plot.scatter('x_mRNA_data', 'y_mRNA_data', source=krug_cds[5],
-                                color = factor_cmap(subtype, five_subtype_colors, five_subtypes), legend_group = subtype)
-
-    me_rna_rna_scatter_plot = figure(title='', x_axis_label=f'{GENE1} mRNA z-score', y_axis_label=f'{GENE2} mRNA z-score',
+    me_rna_rna_scatter_plot = figure(title='NDUFS2-NDUFS3', x_axis_label='mRNA z-score', y_axis_label='mRNA z-score',
                                 plot_width=SCATTER_PLOT_WIDTH, plot_height=SCATTER_PLOT_HEIGHT)
-    me_rna_rna_scatter_plot.scatter('x_mRNA_data', 'y_mRNA_data', source=mertins_cds[5],
-                                color = factor_cmap(subtype, five_subtype_colors[0:4], five_subtypes[0:4]), legend_group = subtype)
 
     return jo_rna_rna_scatter_plot, kr_rna_rna_scatter_plot, me_rna_rna_scatter_plot
 
 def Johansson_RNA_Pro_Scatter_Plot():
-    """ mRNA-Protein scatter plot for gene 1-4 for Johansson"""
-    jo_plot_mRNA_prot1 = figure(title='', x_axis_label=f'{GENE1} mRNA z-score', y_axis_label=f'{GENE1} protein z-score',
-                                plot_width=SCATTER_PLOT_WIDTH, plot_height=SCATTER_PLOT_HEIGHT)
-    jo_plot_mRNA_prot1.scatter(mRNA_data, protein_data, source=johansson_cds[0],
-                               color=factor_cmap(subtype, five_subtype_colors, five_subtypes), legend_group=subtype)
+    """ mRNA-Protein scatter plot figures initiation"""
 
-    jo_plot_mRNA_prot2 = figure(title='', x_axis_label=f'{GENE2} mRNA z-score', y_axis_label=f'{GENE2} protein z-score',
+    jo_plot_mRNA_prot1 = figure(title='NDUFS2', x_axis_label='mRNA z-score', y_axis_label='protein z-score',
                                 plot_width=SCATTER_PLOT_WIDTH, plot_height=SCATTER_PLOT_HEIGHT)
-    jo_plot_mRNA_prot2.scatter(mRNA_data, protein_data, source=johansson_cds[1],
-                               color=factor_cmap(subtype, five_subtype_colors, five_subtypes), legend_group=subtype)
-
-    jo_plot_mRNA_prot3 = figure(title='', x_axis_label=f'{GENE3} mRNA z-score', y_axis_label=f'{GENE3} protein z-score',
+    jo_plot_mRNA_prot2 = figure(title='NDUFS3', x_axis_label='mRNA z-score', y_axis_label='protein z-score',
                                 plot_width=SCATTER_PLOT_WIDTH, plot_height=SCATTER_PLOT_HEIGHT)
-    jo_plot_mRNA_prot3.scatter(mRNA_data, protein_data, source=johansson_cds[2],
-                               color=factor_cmap(subtype, five_subtype_colors, five_subtypes), legend_group=subtype)
-
-    jo_plot_mRNA_prot4 = figure(title='', x_axis_label=f'{GENE4} mRNA z-score', y_axis_label=f'{GENE4} protein z-score',
+    jo_plot_mRNA_prot3 = figure(title='NDUFS7', x_axis_label='mRNA z-score', y_axis_label='protein z-score',
                                 plot_width=SCATTER_PLOT_WIDTH, plot_height=SCATTER_PLOT_HEIGHT)
-    jo_plot_mRNA_prot4.scatter(mRNA_data, protein_data, source=johansson_cds[3],
-                               color=factor_cmap(subtype, five_subtype_colors, five_subtypes), legend_group=subtype)
+    jo_plot_mRNA_prot4 = figure(title='blank 4', x_axis_label='mRNA z-score', y_axis_label='protein z-score',
+                                plot_width=SCATTER_PLOT_WIDTH, plot_height=SCATTER_PLOT_HEIGHT)
 
     return jo_plot_mRNA_prot1, jo_plot_mRNA_prot2, jo_plot_mRNA_prot3, jo_plot_mRNA_prot4
 
 
 def Krug_RNA_Pro_Scatter_Plot():
-    """ mRNA-Protein scatter plot for gene 1-4 for Krug"""
+    """ mRNA-Protein scatter plot figures initiation"""
 
-    kr_plot_mRNA_prot1 = figure(title='', x_axis_label=f'{GENE1} mRNA z-score', y_axis_label=f'{GENE1} protein z-score',
+    kr_plot_mRNA_prot1 = figure(title='NDUFS2', x_axis_label='mRNA z-score', y_axis_label='protein z-score',
                                 plot_width=SCATTER_PLOT_WIDTH, plot_height=SCATTER_PLOT_HEIGHT)
-    kr_plot_mRNA_prot1.scatter(mRNA_data, protein_data, source=krug_cds[0],
-                               color=factor_cmap(subtype, five_subtype_colors, five_subtypes), legend_group=subtype)
-
-    kr_plot_mRNA_prot2 = figure(title='', x_axis_label=f'{GENE2} mRNA z-score', y_axis_label=f'{GENE2} protein z-score',
+    kr_plot_mRNA_prot2 = figure(title='NDUFS3', x_axis_label='mRNA z-score', y_axis_label='protein z-score',
                                 plot_width=SCATTER_PLOT_WIDTH, plot_height=SCATTER_PLOT_HEIGHT)
-    kr_plot_mRNA_prot2.scatter(mRNA_data, protein_data, source=krug_cds[1],
-                               color=factor_cmap(subtype, five_subtype_colors, five_subtypes), legend_group=subtype)
-
-    kr_plot_mRNA_prot3 = figure(title='', x_axis_label=f'{GENE3} mRNA z-score', y_axis_label=f'{GENE3} protein z-score',
+    kr_plot_mRNA_prot3 = figure(title='NDUFS7', x_axis_label='mRNA z-score', y_axis_label='protein z-score',
                                 plot_width=SCATTER_PLOT_WIDTH, plot_height=SCATTER_PLOT_HEIGHT)
-    kr_plot_mRNA_prot3.scatter(mRNA_data, protein_data, source=krug_cds[2],
-                               color=factor_cmap(subtype, five_subtype_colors, five_subtypes), legend_group=subtype)
-
-    kr_plot_mRNA_prot4 = figure(title='', x_axis_label=f'{GENE4} mRNA z-score', y_axis_label=f'{GENE4} protein z-score',
+    kr_plot_mRNA_prot4 = figure(title='blank 4', x_axis_label='mRNA z-score', y_axis_label='protein z-score',
                                 plot_width=SCATTER_PLOT_WIDTH, plot_height=SCATTER_PLOT_HEIGHT)
-    kr_plot_mRNA_prot4.scatter(mRNA_data, protein_data, source=krug_cds[3],
-                               color=factor_cmap(subtype, five_subtype_colors, five_subtypes), legend_group=subtype)
 
     return kr_plot_mRNA_prot1, kr_plot_mRNA_prot2, kr_plot_mRNA_prot3, kr_plot_mRNA_prot4
 
 
 def Mertins_RNA_Pro_Scatter_Plot():
-    """ mRNA-Protein scatter plot for gene 1-4 for Mertins"""
+    """ mRNA-Protein scatter plot figures initiation"""
 
-    me_plot_mRNA_prot1 = figure(title='', x_axis_label=f'{GENE1} mRNA z-score', y_axis_label=f'{GENE1} protein z-score',
+    me_plot_mRNA_prot1 = figure(title='NDUFS2', x_axis_label='mRNA z-score', y_axis_label='protein z-score',
                                 plot_width=SCATTER_PLOT_WIDTH, plot_height=SCATTER_PLOT_HEIGHT)
-    me_plot_mRNA_prot1.scatter(mRNA_data, protein_data, source=mertins_cds[0],
-                               color=factor_cmap(subtype, five_subtype_colors[0:4], five_subtypes[0:4]),
-                               legend_group=subtype)
 
-    me_plot_mRNA_prot2 = figure(title='', x_axis_label=f'{GENE2} mRNA z-score', y_axis_label=f'{GENE2} protein z-score',
+    me_plot_mRNA_prot2 = figure(title='NDUFS3', x_axis_label='mRNA z-score', y_axis_label='protein z-score',
                                 plot_width=SCATTER_PLOT_WIDTH, plot_height=SCATTER_PLOT_HEIGHT)
-    me_plot_mRNA_prot2.scatter(mRNA_data, protein_data, source=mertins_cds[1],
-                               color=factor_cmap(subtype, five_subtype_colors[0:4], five_subtypes[0:4]),
-                               legend_group=subtype)
 
-    me_plot_mRNA_prot3 = figure(title='', x_axis_label=f'{GENE3} mRNA z-score', y_axis_label=f'{GENE3} protein z-score',
+    me_plot_mRNA_prot3 = figure(title='NDUFS7', x_axis_label='mRNA z-score', y_axis_label='protein z-score',
                                 plot_width=SCATTER_PLOT_WIDTH, plot_height=SCATTER_PLOT_HEIGHT)
-    me_plot_mRNA_prot3.scatter(mRNA_data, protein_data, source=mertins_cds[2],
-                               color=factor_cmap(subtype, five_subtype_colors[0:4], five_subtypes[0:4]),
-                               legend_group=subtype)
 
-    me_plot_mRNA_prot4 = figure(title='', x_axis_label=f'{GENE4} mRNA z-score', y_axis_label=f'{GENE4} protein z-score',
+    me_plot_mRNA_prot4 = figure(title='blank 4', x_axis_label='mRNA z-score', y_axis_label='protein z-score',
                                 plot_width=SCATTER_PLOT_WIDTH, plot_height=SCATTER_PLOT_HEIGHT)
-    me_plot_mRNA_prot4.scatter(mRNA_data, protein_data, source=mertins_cds[3],
-                               color=factor_cmap(subtype, five_subtype_colors[0:4], five_subtypes[0:4]),
-                               legend_group=subtype)
 
     return me_plot_mRNA_prot1, me_plot_mRNA_prot2, me_plot_mRNA_prot3, me_plot_mRNA_prot4
