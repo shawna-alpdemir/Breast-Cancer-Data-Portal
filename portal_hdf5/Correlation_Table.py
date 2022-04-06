bea03684ff497cfde93ada44a28cc327ee15c4d3
kenlzh1996 <zl292@cornell.edu>
2022-04-06 11:30:46 -0400

    updated file path

--- portal_hdf5/Correlation_Table.py
+++ portal_hdf5/Correlation_Table.py
@@ -4,12 +4,12 @@ import numpy as np
 import scipy.stats as sc
 import time
 
-JohanssonProteome = '/Users/zhuoheng/Desktop/Vacanti/RawData_March/hdf5/JohanssonProteome.hdf5'
-JohanssonTranscriptome = '/Users/zhuoheng/Desktop/Vacanti/RawData_March/hdf5/JohanssonTranscriptome.hdf5'
-KrugProteome = '/Users/zhuoheng/Desktop/Vacanti/RawData_March/hdf5/KrugProteome.hdf5'
-KrugTranscriptome = '/Users/zhuoheng/Desktop/Vacanti/RawData_March/hdf5/KrugTranscriptome.hdf5'
-MertinsProteome = '/Users/zhuoheng/Desktop/Vacanti/RawData_March/hdf5/MertinsProteome.hdf5'
-MertinsTranscriptome = '/Users/zhuoheng/Desktop/Vacanti/RawData_March/hdf5/MertinsTranscriptome.hdf5'
+JohanssonProteome = 'portal_hdf5/Data/HDF5/JohanssonProteome.hdf5'
+JohanssonTranscriptome = 'portal_hdf5/Data/HDF5/JohanssonTranscriptome.hdf5'
+KrugProteome = 'portal_hdf5/Data/HDF5/KrugProteome.hdf5'
+KrugTranscriptome = 'portal_hdf5/Data/HDF5/KrugTranscriptome.hdf5'
+MertinsProteome = 'portal_hdf5/Data/HDF5/MertinsProteome.hdf5'
+MertinsTranscriptome = 'portal_hdf5/Data/HDF5/MertinsTranscriptome.hdf5'
 
 def Get_Protein_Correlation_Table(gene_name):
 
